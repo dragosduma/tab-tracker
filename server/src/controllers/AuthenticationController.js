5ae4c4ecd3f43d69438911b58df7af5b0f2e5956
dragos <dragos.duma@gmail.com>
2022-04-27 19:36:56 +0300

    Added joi validation on email and password fields

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -7,7 +7,7 @@ module.exports = {
             res.send(user.toJSON())
         } catch (err) {
             res.status(400).send({
-                error: 'This email account is already in user.'
+                error: 'This email account is already in use.'
             })
         }
     }
